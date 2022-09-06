25e5077605055b04a65e3aceb679e784dd0c9f40
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-06 13:27:13 +0300

    - Added Identity Server to web application pipeline - Configured identity server for web application pipeline

--- src/Services/Identity/Mango.Services.Identity/Program.cs
+++ src/Services/Identity/Mango.Services.Identity/Program.cs
@@ -1,6 +1,35 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.Context;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(IdentityConfig.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+    .AddInMemoryClients(IdentityConfig.Clients)
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddDeveloperSigningCredential();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -18,6 +47,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
